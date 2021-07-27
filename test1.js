67e33980e7df934b77dc3e7c1898d804b814da51
Brittney Coble <bwilcutt#gmail.com>
2021-07-26 22:58:24 -0400

    learning Go and JS for testcafe

---
+++ test1.js
@@ -0,0 +1,26 @@
+import { Selector } from 'testcafe';
+
+function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('watching page');
+      }, 2000);
+    });
+  }
+
+  fixture `Getting Started`
+    .page `http://devexpress.github.io/testcafe/example`;
+
+test('My first test', async t => {
+    await t
+    .typeText('#developer-name', 'John Smith')
+    .click('#submit-button');
+
+    const articleHeader = await Selector('.result-content').find('h1');
+
+    // Obtain the text of the article header
+    let headerText = await articleHeader.innerText;
+
+    const result = await resolveAfter2Seconds();
+    console.log(result);
+});
